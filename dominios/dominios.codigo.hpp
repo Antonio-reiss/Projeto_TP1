53529aa2df586384874abf760f0a544e272596ae
esteerr234 <ester4944@gmail.com>
2025-10-15 21:15:28 -0300

    Refactor Codigo class constructor and methods

--- dominios/dominios.codigo.hpp
+++ dominios/dominios.codigo.hpp
@@ -7,9 +7,10 @@ class Codigo{
 private:
     string valor;
 public:
-    Codigo(string v);
-    void setValor (string v);
+    Codigo(string valor);
+    void setValor (string valor);
     string getValor() const;
+
 };
 
 #endif // DOMINIOS_CODIGO_HPP_INCLUDED
