2b3a32406a3dc88705624b5f83ca0c3a69e33c1d
Antonio Reis <antoniolucas.reiss@gmail.com>
2025-10-24 21:21:25 -0300

    Alteracao de entidade pessoa

--- dominios/main.cpp
+++ dominios/main.cpp
@@ -1,39 +1,18 @@
-#include "../entidades/entidade.gerente.hpp"
-#include "dominios.ramal.hpp"
+#include "../entidades/entidade.pessoa.hpp"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main(){
-    int ramal;
-    string nome, email, senha;
+    string email, nome;
 
-    cout << "Digite seu nome" << endl;
     cin >> nome;
-
-    cout << "Digite seu email" << endl;
     cin >> email;
 
-    cout << "Digite o ramal" << endl;
-    cin >> ramal;
+    Pessoa alguem(nome, email);
 
-    cout << "Digite uma senha" << endl;
-    cin >> senha;
-
-    Gerente gerente(nome, email, ramal, senha);
-
-    cout << "Gerente:" << endl;
-    cout << "Nome:" << endl;
-    cout << gerente.getNome() << endl;
-    cout << endl;
-    cout << "Email:" << endl;
-    cout << gerente.getEmail() << endl;
-    cout << "Ramal:" << endl;
-    cout << gerente.getRamal() << endl;
-    cout << "Senha:" << endl;
-    cout << gerente.getSenha() << endl;
-    cout << endl;
+    cout << alguem.getEmail() << endl << alguem.getNome();
 
     return 0;
 }
