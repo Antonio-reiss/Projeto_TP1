fdbcb0b9fdae3ab7e56a5d864d5b35cbdb6b1da1
Mellen15 <guedesellen91@gmail.com>
2025-11-14 08:42:25 -0300

    Documentação da camada de apresentação Gerente

--- Subsistemas/Gerente/ia.gerente.hpp
+++ Subsistemas/Gerente/ia.gerente.hpp
@@ -1,6 +1,7 @@
 /**
 * @file ia.gerente.hpp
-* @brief
+* @brief Interface abstrata para a camada de apresentação do módulo gerente.
+* @author Maria Ellen Guedes Montalvão - 2232011402
 */
 #ifndef IA_GERENTE_HPP
 #define IA_GERENTE_HPP
@@ -12,8 +13,23 @@
 
 using namespace std;
 
+/**
+* @class iaGerente
+* @brief Contrato para o módulo de apresentação do Gerente.
+*
+* Esta interface define os métodos que a IU deve implementar para coletar dados e iniciar a validação de domínio.
+*/
 class iaGerente{
 public:
+    /**
+    * @brief Valida os dados de entrada para a criação de uma conta Gerente.
+    * * O método usa os construtores de domínio (Nome, Email, Ramal, Senha) para a validação.
+    * @param string nome com o nome de entrada.
+    * @param string email com o email de entrada.
+    * @param int ramal com o ramal de entrada.
+    * @param string senha com a senha de entrada.
+    * @return Retorna true se ocorrer a validação em todos os domínios forem bem-sucedidas, false caso contrário.
+    */
     virtual bool validarConta(const string&, const string&, const int&, const string&) = 0;
 
 };
--- Subsistemas/IU/iu.hpp
+++ Subsistemas/IU/iu.hpp
@@ -1,6 +1,8 @@
 /**
 * @file iu.hpp
 * @brief Definição da interface do módulo principal de apresentação (IU).
+*
+* Esse módulo lida com o menu principal e o controle de fluxo do sistema.
 * @author Maria Ellen Guedes Montalvão - 232011402
 */
 #ifndef IU_HPP_INCLUDED
@@ -9,6 +11,7 @@
 
 /**
 * @brief Contém o menu que inicializa o sistema.
+* * Responsável por receber a opcao do usuário e delegar o fluxo.
 */
 void iniciarSistema();
 
