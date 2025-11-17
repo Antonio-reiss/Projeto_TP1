6d43656b02279138d568dc7f7deedefcc1b53a04
esteerr234 <ester4944@gmail.com>
2025-11-17 19:57:00 -0300

    Update ia.hospede.hpp

--- Subssitema/ia.hospede.hpp
+++ Subssitema/ia.hospede.hpp
@@ -1,14 +1,38 @@
+/**
+ * @file ia_hospede.hpp
+ * @brief Interface que define operacoes para manipulacao de hospedes no sistema de gestao hoteleira.
+ *
+ * Esta interface e utilizada para validar operacoes de criar, editar e excluir hospedes
+ * dentro do sistema. As classes que implementam esta interface devem garantir que as
+ * regras de negocio especificas sejam seguidas.
+ *
+ * @author Ester Andrade Sousa - 242012109
+ */
 #ifndef IA_HOSPEDE_HPP_INCLUDED
 #define IA_HOSPEDE_HPP_INCLUDED
 #include <string>
 
 using namespace std;
-
+/**
+ * @class IAHospede
+ * @brief Interface responsavel por definir metodos de validacao de hospedes.
+ *
+ * Classes que implementam esta interface devem tratar a validacao dos dados
+ * para operacoes de criacao, edicao e exclusao de hospedes.
+ */
 class IAHospede {
 public:
+/**
+     * @brief Valida os dados de criacao de um hospede.
+     * @param nome String com o nome do hospede.
+     * @param email String com o email do hospede.
+     * @param telefone String com o telefone do hospede.
+     * @return true se todos os dados forem validos, false caso contrario.
+     */
     virtual bool validarCriar(string&, string&, string&, string&) = 0;
     virtual bool validarEditar(string&, string&, string&) = 0;
     virtual bool validarExcluir(string&) = 0;
 };
 
 #endif // IA_HOSPEDE_HPP_INCLUDED
+
