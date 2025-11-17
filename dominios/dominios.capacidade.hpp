252cf97c8a037ae10f416d0c324e68fc0efb1729
esteerr234 <ester4944@gmail.com>
2025-11-17 19:39:12 -0300

    Update dominios.capacidade.hpp

--- dominios/dominios.capacidade.hpp
+++ dominios/dominios.capacidade.hpp
@@ -1,3 +1,9 @@
+/**
+* @file dominios.capacidade.hpp
+* @brief Definicao da classe Capacidade, que valida a capacidade de um quarto.
+*
+* @author Ester Andrade Sousa - 242012109
+*/
 #ifndef DOMINIOS_HPP_INCLUDED
 #define DOMINIOS_HPP_INCLUDED
 
@@ -5,12 +11,30 @@
 
 using namespace std;
 
+/**
+* @class Capacidade
+* @brief Representa e valida a capacidade de um quarto (1 a 4 hospedes).
+*/
 class Capacidade {
 private:
     int valor;
 public:
+    /**
+    * @brief Construtor da classe Capacidade.
+    * @param valor Inteiro representando a capacidade inicial.
+    * @throws invalid_argument se a capacidade for invalida.
+    */
     Capacidade (int valor);
+/**
+    * @brief Define e valida um novo valor para a capacidade.
+    * @param valor Novo valor entre 1 e 4.
+    * @throws invalid_argument se o valor estiver fora do intervalo.
+    */
     void setValor (int valor);
+ /**
+    * @brief Retorna o valor da capacidade.
+    * @return Valor inteiro da capacidade.
+    */
     int getValor () const;
 
 
