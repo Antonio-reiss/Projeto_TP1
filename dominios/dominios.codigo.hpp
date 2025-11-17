ddc80358de60b7ce5a98f15ea91159760b99137d
esteerr234 <ester4944@gmail.com>
2025-11-17 19:44:07 -0300

    Update dominios.codigo.hpp

--- dominios/dominios.codigo.hpp
+++ dominios/dominios.codigo.hpp
@@ -1,14 +1,39 @@
+/**
+* @file dominios.codigo.hpp
+* @brief Definicao da classe Codigo, que representa um codigo alfanumerico de 10 caracteres.
+*
+* @author Ester Andrade Sousa - 242012109
+*/
+
 #ifndef DOMINIOS_CODIGO_HPP_INCLUDED
 #define DOMINIOS_CODIGO_HPP_INCLUDED
 #include<string>
 using namespace std;
+/**
+* @class Codigo
+* @brief Representa e valida um codigo de 10 caracteres contendo letras minusculas e numeros.
+*/
 
 class Codigo{
 private:
     string valor;
 public:
+/**
+    * @brief Construtor da classe Codigo.
+    * @param valor Valor inicial do codigo em string.
+    * @throws invalid_argument se o valor nao for valido.
+    */
     Codigo(string valor);
+/**
+    * @brief Define e valida um novo codigo.
+    * @param valor Novo valor do codigo.
+    * @throws invalid_argument se o codigo nao for valido.
+    */
     void setValor (string valor);
+/**
+    * @brief Retorna o codigo armazenado.
+    * @return String com o valor do codigo.
+    */
     string getValor() const;
 
 };
