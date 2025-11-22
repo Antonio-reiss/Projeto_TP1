441889fc6f3da0b3eec05f047985b4a39b6b9436
esteerr234 <ester4944@gmail.com>
2025-11-22 14:58:24 -0300

    Update controladora.hospede.hpp

--- Subsistemas/HospedeReserva/controladora.hospede.hpp
+++ Subsistemas/HospedeReserva/controladora.hospede.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file controladora.hospede.hpp
+ * @brief Cabecalho da controladora do modulo de Hospede.
+ *
+ * Declara as funcoes responsaveis por intermediar a interacao entre a
+ * apresentacao e as camadas de logica e servico, incluindo operacoes
+ * de criacao, edicao, listagem e exclusao de hospedes.
+ * @author Ester Andrade Sousa - 242012109
+ */
+
 #ifndef CONTROLADORA_HOSPEDE_HPP_INCLUDED
 #define CONTROLADORA_HOSPEDE_HPP_INCLUDED
 
@@ -8,9 +18,27 @@
 #include <iostream>
 #include <limits>
 
+/**
+ * @brief Exibe o menu de opcoes relacionadas ao modulo de hospede.
+ */
 void opcoesHospede();
+
+/**
+ * @brief Valida e solicita os dados necessarios para criar um novo hospede.
+ *
+ * @return true se os dados forem validados e a criacao for bem sucedida,
+ * false caso contrario.
+ */
 bool validarCriarHospede();
+
+/**
+ * @brief Solicita e valida os dados necessarios para editar um hospede.
+ */
 void validarEditarHospede();
+
+/**
+ * @brief Realiza o processo de exclusao de um hospede com base no email informado.
+ */
 void excluirHospede();
 
-#endif
+#endif // CONTROLADORA_HOSPEDE_HPP_INCLUDED
