3befeb0b62bbe4141760841a6d8ddf3cb0c5d37f
esteerr234 <ester4944@gmail.com>
2025-11-22 14:53:04 -0300

    Update is.hospede.hpp

--- Subsistemas/HospedeReserva/is.hospede.hpp
+++ Subsistemas/HospedeReserva/is.hospede.hpp
@@ -1,9 +1,9 @@
 /**
  * @file is.hospede.hpp
- * @brief Interface abstrata para o serviço de gerenciamento de hóspedes.
+ * @brief Interface abstrata para o servico de gerenciamento de hospedes.
  *
- * Define o contrato para criação, listagem, edição e exclusão de hóspedes,
- * permitindo que diferentes implementações mantenham a mesma estrutura lógica.
+ * Define o contrato para criacao, listagem, edicao e exclusao de hospedes,
+ * permitindo que diferentes implementacoes mantenham a mesma estrutura logica.
  * @author Ester Andrade Sousa - 242012109
  */
 
@@ -13,54 +13,52 @@
 #include <string>
 using namespace std;
 
-
-
 /**
  * @class isHospede
- * @brief Interface que define as operações essenciais do CRUD de hóspede.
+ * @brief Interface que define as operacoes essenciais do CRUD de hospede.
  *
- * As classes que implementarem esta interface devem garantir validação dos
- * dados de entrada e integração com o mecanismo de persistencia adotado.
+ * As classes que implementarem esta interface devem garantir validacao dos
+ * dados de entrada e integracao com o mecanismo de persistencia adotado.
  */
 class isHospede {
 public:
 
     /**
-     * @brief Cria um novo hóspede no sistema.
+     * @brief Cria um novo hospede no sistema.
      *
-     * @param nome nome do hóspede.
-     * @param email email do hóspede (chave primária).
-     * @param endereco endereço do hóspede.
-     * @param cartao número do cartão associado.
+     * @param nome nome do hospede.
+     * @param email email do hospede (chave primaria).
+     * @param endereco endereco do hospede.
+     * @param cartao numero do cartao associado.
      *
-     * @return true se o cadastro for concluído com sucesso, false em falhas.
+     * @return true se o cadastro for concluido com sucesso, false em falhas.
      */
     virtual bool criarHospede(const string&, const string&, const string&, const string&) = 0;
 
     /**
-     * @brief Lista todos os hóspedes cadastrados.
+     * @brief Lista todos os hospedes cadastrados.
      *
-     * @return true se a operação ocorrer corretamente, false caso contrário.
+     * @return true se a operacao ocorrer corretamente, false caso contrario.
      */
     virtual bool listarHospedes() = 0;
 
     /**
-     * @brief Edita informações de um hóspede existente.
+     * @brief Edita informacoes de um hospede existente.
      *
-     * @param emailChave email que identifica o hóspede a ser atualizado.
-     * @param novoEndereco novo valor para o campo endereço.
-     * @param novoCartao novo valor para o campo cartão.
+     * @param emailChave email que identifica o hospede a ser atualizado.
+     * @param novoEndereco novo valor para o campo endereco.
+     * @param novoCartao novo valor para o campo cartao.
      *
-     * @return true se a edição for bem-sucedida, false em erro.
+     * @return true se a edicao for bem sucedida, false em erro.
      */
     virtual bool editarHospede(string, string, string) = 0;
 
     /**
-     * @brief Exclui um hóspede do sistema.
+     * @brief Exclui um hospede do sistema.
      *
-     * @param email email do hóspede que será removido.
+     * @param email email do hospede que sera removido.
      *
-     * @return true se a exclusão for concluída, false caso contrário.
+     * @return true se a exclusao for concluida, false caso contrario.
      */
     virtual bool excluirHospede(const string&) = 0;
 };
